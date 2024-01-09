c7df3a9e5a326e7f10cfcd8b38e59fa6db601737
Happy0503 <90928071+Happy0503@users.noreply.github.com>
2024-01-09 17:05:27 +0100

    Update d1.sh

--- d1.sh
+++ d1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#Vous devez récupérer la liste des conducteurs avec le nombre de trajets différents qu’ils ont effectués. Depuis cette liste, triée par ordre 
+#décroissant de nombre de trajets, vous ne devez garder que les 10 premiers conducteurs.
+
 
 a
