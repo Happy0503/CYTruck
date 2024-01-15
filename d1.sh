cbd785e2c8cb599622b8f73be1a30bfce9d01c18
Happy0503 <90928071+Happy0503@users.noreply.github.com>
2024-01-15 09:59:21 +0100

    Update d1.sh

--- d1.sh
+++ d1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 #Vous devez récupérer la liste des conducteurs avec le nombre de trajets différents qu’ils ont effectués. Depuis cette liste, triée par ordre 
 #décroissant de nombre de trajets, vous ne devez garder que les 10 premiers conducteurs.
 
 
-a
+
+# DATA.CVS NOM A CHANGER : RECUP ARG 1
+time cat ./data/data.csv | cut -f1,6 -d';' | sort | uniq | cut -f2 -d';' | sort | uniq -c | sort -n -r | head -10
+
