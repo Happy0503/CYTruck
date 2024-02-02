147db2a00d678a5fdf9e42137c7f08a5a0e03ea5
Happy0503 <90928071+Happy0503@users.noreply.github.com>
2024-02-02 12:28:20 +0100

    Delete d2.sh

--- d2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-#Récupère les 10 conducteurs ayant la plus grande distance parcourue par ordre décroissant.
-time cat ./data/data.csv | sed -e 1d | cut -f5,6 -d';' | ./d2 | sort -n -r | head -10 > ./temp/result_d2.txt
-# DATA.CVS NOM A CHANGER : RECUP ARG 1
-
-
-#time cat ./data/data.csv | sed -e 1d | cut -f5,6 -d';' | ./d2 > ./temp/result_d2.txt
