830f5f3f1981be5f4160f594d9747737bf83f909
Happy0503 <90928071+Happy0503@users.noreply.github.com>
2024-02-02 14:46:29 +0100

    Update l.sh

--- l.sh
+++ l.sh
@@ -2,12 +2,8 @@
 
 
 #Récupère les 10 trajets aux distances les plus longues par ordre croissant.
-time cat ./data/data.csv | cut -f1,5 -d';' | sort | ./progc/l | sort -n -r | head -10 | sort -n > ./temp/result_l.txt
+time cat ./data/data.csv | cut -f1,5 -d';' | sort | ./progc/l | sort -n -r | head -10 | sort -t";" -nk 2 > ./temp/result_l.txt
 # DATA.CVS NOM A CHANGER : RECUP ARG 1
-
-
-
-
 
 
 
